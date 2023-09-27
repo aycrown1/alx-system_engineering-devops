ac3169d1ab25b6fc7ee80488da7dcb18847f14a9
aycrown1 <ayoadeadedamola198@gmail.>
2023-09-27 21:34:40 +0100

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,49 @@
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Create Nginx configuration file
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "server {
+    listen 80;
+    server_name _;
+
+    location / {
+        root /var/www/html;
+        index index.html;
+    }
+
+    location /redirect_me {
+        return 301 https://www.example.com/new_location;
+    }
+}",
+  notify  => Service['nginx'],
+}
+
+# Create the document root directory
+file { '/var/www/html':
+  ensure => directory,
+}
+
+# Create an index.html file with the content "Hello World!"
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+}
+
+# Enable Nginx site
+exec { 'enable_nginx_site':
+  command => 'ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/',
+  creates => '/etc/nginx/sites-enabled/default',
+  require => [Package['nginx'], File['/etc/nginx/sites-available/default']],
+  notify  => Service['nginx'],
+}
+
+# Start the service
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Exec['enable_nginx_site'],
+}
