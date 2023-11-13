ee7a4bf606b412f4eb06c66212dba590ae062ee9
aycrown1 <ayoadeadedamola198@gmail.>
2023-11-13 12:11:36 +0100

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # The error 500 was cause by a wrong extention of the `class-wp-locale.php` file
 # our manifest is to replace the occurance of `phpp` in the /var/www/html/wp-settings.php
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
+exec { 'replace_phpp_with_php':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
 }
