31d798c255b43e00e8515dd0341d49aeb9e0bb25
aycrown1 <ayoadeadedamola198@gmail.>
2023-11-17 03:21:01 +0100

    User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+# Increase the desired open file limits for the holberton user
+exec { 'update_limits_conf':
+  command     => 'sudo sed -iE -e "s/^holberton hard nofile 5/holberton hard nofile 4096/" -e "s/^holberton soft nofile 4/holberton soft nofile 4096/" /etc/security/limits.conf',
+  path        => ['/bin/', '/usr/bin/'],
+}
